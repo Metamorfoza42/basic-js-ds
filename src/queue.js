fceec06b80f0e6192a4141badfda60f2c9750e21
Iurii <149243308+Metamorfoza42@users.noreply.github.com>
2023-12-25 02:13:31 +0400

    feat: solve queue task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    let node = new ListNode(value);
+
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.length === 0) return null;
+
+    let outOfQueue = this.head.value;
+    this.head = this.head.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return outOfQueue;
   }
 }
 
 module.exports = {
   Queue
 };
+
+const queue = new Queue();
+
+// queue.enqueue(1);
+// queue.enqueue('cat');
+// queue.enqueue('dolphin');
+// console.log(queue);
+
+// console.log(queue.dequeue());
+// console.log(queue);
+
+// console.log(queue.getUnderlyingList());
